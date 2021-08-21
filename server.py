ec2ff2588d9c385e8e9a3f74f008361f86e4b54e
Axel Demborg <axel.demborg@gmail.com>
2021-08-22 00:05:50 +0200

    draw a cat!

--- server.py
+++ server.py
@@ -1,19 +1,44 @@
 from math import sin, cos, pi
 from typing import Sequence, Tuple
+from pathlib import Path
 
 import serial
 import numpy as np
 import cv2
 
-def get_circle(radius: float) -> Tuple[float, float]:
+def get_circle(radius: float) -> Sequence[Tuple[float, float]]:
     return (
         (16 + sin(angle) * radius, 15 - cos(angle) * radius)
         for angle in np.linspace(0, 2*pi, 100)
     )
 
+def get_contour(img_path: str) -> Sequence[Tuple[float, float]]:
+    img = cv2.imread(img_path)
+    img = cv2.resize(img, (256, 256))
+
+    # Our operations on the frame come here
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    cv2.imshow('frame',gray)
+    blur = cv2.GaussianBlur(gray,(11,11),0)
+    cv2.imshow('blur',blur)
+    ret, th = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    cv2.imshow('bin',th)
+    contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+    edges = np.zeros(gray.shape)
+    cv2.drawContours(edges, contours, -1, 255, 3)
+    cv2.imshow('contour',edges)
+    #cv2.waitKey(0)
+    contours = np.concatenate(contours).astype('float32').squeeze()
+    contours[:, 0] /= img.shape[0]
+    contours[:, 1] /= img.shape[1]
+    contours *= 20
+    contours += 6
+    cv2.waitKey(0)
+    return contours
+
 def main():
-    with serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1) as arduino:
-        for x, y in get_circle(10):
+    with serial.Serial(port="/dev/ttyACM0", baudrate=9600) as arduino:
+        for x, y in get_contour("cat.jpg"):
             print(f"going to: ({x:.2f}, {y:.2f})")
             arduino.write(f"{x:.2f},{y:.2f};".encode())
 
