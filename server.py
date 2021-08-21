c83412d7ec2cd775d729102c3fc35e109e1fa67a
Axel Demborg <axel.demborg@gmail.com>
2021-08-21 23:11:45 +0200

    initial server for drawing circle

---
+++ server.py
@@ -0,0 +1,28 @@
+from math import sin, cos, pi
+from typing import Tuple
+from time import sleep
+
+import serial
+import numpy as np
+
+def get_coordinates(angle: float, radius: float) -> Tuple[float, float]:
+    return (16 + sin(angle) * radius, 15 - cos(angle) * radius)
+
+
+def main():
+    with serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1) as arduino:
+        for angle in np.linspace(0, 2*pi, 100):
+            x, y = get_coordinates(
+                angle,
+                10,
+            )
+            print(f"going to: ({x:.2f}, {y:.2f})")
+            arduino.write(f"{x:.2f},{y:.2f};".encode())
+            # sleep(0.5)
+
+            print(f"received: {arduino.readline().decode('ascii')}")
+
+        print("Closing port")
+
+if __name__ == "__main__":
+    main()
