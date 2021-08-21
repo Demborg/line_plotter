8dbaeed8158293225ae761eb73b6eb7c5c28fe82
Axel Demborg <axel.demborg@gmail.com>
2021-08-21 23:11:21 +0200

    Take position as float

--- line_plotter.ino
+++ line_plotter.ino
@@ -12,10 +12,10 @@ AccelStepper left_stepper(AccelStepper::FULL4WIRE, 8, 10, 9, 11);
 AccelStepper right_stepper(AccelStepper::FULL4WIRE, 4, 6, 5, 7);
 
 const int steps_per_cm = 102;
-const int spool_distance = 32;
-const int calibration_distance = 16;
-int x = 16;
-int y = 0;
+const float spool_distance = 32;
+const float calibration_distance = 16;
+float x = 16;
+float y = 0;
 
 // Up to 10 steppers can be handled as a group by MultiStepper
 MultiStepper steppers;
@@ -30,28 +30,27 @@ void setup() {
   // Then give them to MultiStepper to manage
   steppers.addStepper(left_stepper);
   steppers.addStepper(right_stepper);
-
-  delay(5000);
 }
 
-long* calculatePosition(long x, long y) {
-  Serial.println(x);
-  Serial.println(y);
+long* calculatePosition(float x, float y) {
+  //Serial.println(x);
+  //Serial.println(y);
   static long result[2];
-  long other_x = spool_distance - x;
-  result[0] = (sqrt(x*x + y*y) - calibration_distance) * steps_per_cm;
-  result[1] = -(sqrt(other_x*other_x + y*y) - calibration_distance) * steps_per_cm;
-  Serial.println(result[0]);
-  Serial.println(result[1]);
+  float other_x = spool_distance - x;
+  result[0] = round((sqrt(x*x + y*y) - calibration_distance) * steps_per_cm);
+  result[1] = round(-(sqrt(other_x*other_x + y*y) - calibration_distance) * steps_per_cm);
+  //Serial.println(result[0]);
+  //Serial.println(result[1]);
   return result;
 }
 
 void loop() {
   if (Serial.available() > 0){
-    x = Serial.parseInt();
-    y = Serial.parseInt();
+    x = Serial.parseFloat();
+    y = Serial.parseFloat();
 
     steppers.moveTo(calculatePosition(x, y));
     steppers.runSpeedToPosition(); // Blocks until all are in position
+    Serial.println((String)"At x=" + x + " y=" + y);
   }
 }
