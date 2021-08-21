f8ccde1de985b5ea8ac8119a4f0b31c699f14e66
Axel Demborg <axel.demborg@gmail.com>
2021-08-21 23:26:45 +0200

    Refactor: make get_cricle return full circle

--- server.py
+++ server.py
@@ -1,20 +1,19 @@
 from math import sin, cos, pi
-from typing import Tuple
+from typing import Sequence, Tuple
 
 import serial
 import numpy as np
+import cv2
 
-def get_coordinates(angle: float, radius: float) -> Tuple[float, float]:
-    return (16 + sin(angle) * radius, 15 - cos(angle) * radius)
-
+def get_circle(radius: float) -> Tuple[float, float]:
+    return (
+        (16 + sin(angle) * radius, 15 - cos(angle) * radius)
+        for angle in np.linspace(0, 2*pi, 100)
+    )
 
 def main():
     with serial.Serial(port="/dev/ttyACM0", baudrate=9600, timeout=1) as arduino:
-        for angle in np.linspace(0, 2*pi, 100):
-            x, y = get_coordinates(
-                angle,
-                10,
-            )
+        for x, y in get_circle(10):
             print(f"going to: ({x:.2f}, {y:.2f})")
             arduino.write(f"{x:.2f},{y:.2f};".encode())
 
