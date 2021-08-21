a80e579d06a33003e81e8cf8b4dc6a1fa76c0448
Axel Demborg <axel.demborg@gmail.com>
2021-08-22 00:17:06 +0200

    improved cat painting

--- server.py
+++ server.py
@@ -16,19 +16,15 @@ def get_contour(img_path: str) -> Sequence[Tuple[float, float]]:
     img = cv2.imread(img_path)
     img = cv2.resize(img, (256, 256))
 
-    # Our operations on the frame come here
     gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     cv2.imshow('frame',gray)
-    blur = cv2.GaussianBlur(gray,(11,11),0)
-    cv2.imshow('blur',blur)
-    ret, th = cv2.threshold(blur,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+    ret, th = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     cv2.imshow('bin',th)
     contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     edges = np.zeros(gray.shape)
-    cv2.drawContours(edges, contours, -1, 255, 3)
+    cv2.drawContours(edges, contours[1:], -1, 255, 3)
     cv2.imshow('contour',edges)
-    #cv2.waitKey(0)
-    contours = np.concatenate(contours).astype('float32').squeeze()
+    contours = np.concatenate(contours[1:]).astype('float32').squeeze()
     contours[:, 0] /= img.shape[0]
     contours[:, 1] /= img.shape[1]
     contours *= 20
