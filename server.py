cd8f7f7cfc9210ca507b805d5a14e2d7be2d695c
Axel Demborg <axel.demborg@gmail.com>
2021-08-28 02:01:50 +0200

    Add code for AM modulated line version

--- server.py
+++ server.py
@@ -19,7 +19,7 @@ def read_gray(img_path: str, size=1024):
 
 def draw_contour(contour, size):
     canvas = np.ones((size, size)) * 255
-    cv2.drawContours(canvas, [contour], -1, 0, 1)
+    cv2.drawContours(canvas, [contour.astype("int32")], -1, 0, 1)
     cv2.imshow('contour',canvas)
     cv2.waitKey(0)
 
@@ -33,7 +33,7 @@ def get_contour(img_path: str) -> Sequence[Tuple[float, float]]:
     ret, th = cv2.threshold(gray,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
     cv2.imshow('bin',th)
     contours, hierarchy = cv2.findContours(th,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    contour = np.concatenate(contours[1:]).astype('int32').squeeze()
+    contour = np.concatenate(contours[1:]).squeeze()
     import ipdb; ipdb.set_trace()
     draw_contour(contour, gray.shape[0])
     return contour_to_canvas(contour, gray.shape[0])
@@ -57,15 +57,53 @@ def get_hilbert_curve(img_path: str):
     gray = read_gray(img_path)
     size = gray.shape[0]
     points = np.array(hilbert_recursive((size/2, size/2), size, gray))
-    contour = points.reshape((-1, 1, 2)).astype("int32")
+    contour = points.reshape((-1, 1, 2))
     draw_contour(contour, size)
     return contour_to_canvas(points, size)
+
+
+def get_crow_curve(img_path: str):
+    gray = read_gray(img_path, 1000)
+    size = gray.shape[0]
+    blur = cv2.GaussianBlur(gray, (11, 11), 0)
+
+    x0 = np.random.random((10,2)) * size
+    def func(x):
+        x = x.reshape(-1, 2)
+        canvas = np.ones((size, size)) * 255
+        cv2.drawContours(canvas, [x.astype("int32")], -1, 0, 10)
+        return np.mean((blur - canvas)**2)
+
+    print(f"{x0=}")
+    result = minimize(func, x0, method="Nelder-Mead")
+    print(f"{result=}")
+    contour = result.x.reshape(-1, 2).astype("int32")
+    draw_contour(contour, size)
+    
+
+def get_am_line(img_path: str, lines = 40, samples=500):
+    gray = read_gray(img_path, 1000)
+    size = gray.shape[0]
+    line_width = size // lines 
+    step = size // samples
+    contour = []
+    for i in range(lines):
+        for j in range(samples):
+            x = int(step * j + (i % 2) * (size - step * j * 2 -1))
+            y = int((i + 0.5) * line_width)
+            y += (1 - gray[y, x] / 255) * line_width *0.4  * (1 - 2 * (j % 2))
+            contour.append((x, y))
+    contour = np.array(contour)
+    draw_contour(contour, size)
+    return contour_to_canvas(contour, size)
+
+
 
 
 def main():
     stop = True
     with serial.Serial(port="/dev/ttyACM0", baudrate=9600) as arduino:
-        for x, y in get_hilbert_curve("me.jpg"):
+        for x, y in get_am_line("y.png", lines=20):
             print(f"going to: ({x:.2f}, {y:.2f})")
             arduino.write(f"{x:.2f},{y:.2f};".encode())
             print(f"received: {arduino.readline().decode('ascii')}")
