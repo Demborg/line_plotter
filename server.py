189cb878e2d0036231047295d61b06aa47172ba3
Axel Demborg <axel.demborg@gmail.com>
2021-09-04 01:02:34 +0200

    configure methods crudly from commandline

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import json
 from typing import Sequence, Tuple
 
 import click
@@ -8,8 +9,6 @@ from tqdm import tqdm
 
 OFFSETS = [(-1, -1), (-1, 1), (1, 1), (1, -1)]
 ORDERS = [[0, 3, 2, 1], [0, 1, 2, 3], [0, 1, 2, 3], [2, 1, 0, 3]]
-MAX_DEPTH = 8
-
 
 def read_gray(img_path: str, size=1024):
     img = cv2.imread(img_path)
@@ -36,23 +35,23 @@ def get_contour(gray: np.ndarray) -> Sequence[Tuple[float, float]]:
     return np.concatenate(contours[1:]).squeeze()
 
 
-def hilbert_recursive(point, size, image, depth = 0, offsets = OFFSETS):
-    points = [(point[0] + size / 4 * o0, point[1] + size / 4 * o1) for o0, o1 in offsets]
-    image_slice = image[
-        round(point[1] - size / 2):round(point[1] + size / 2),
-        round(point[0] - size / 2):round(point[0] + size / 2),
-    ]
-    darkness = (255 - np.min(image_slice)) / 255
-    if depth / MAX_DEPTH < np.sqrt(darkness):
-        return [h for i, p in enumerate(points) for h in hilbert_recursive(
-            p, size / 2, image, depth + 1, [offsets[j] for j in ORDERS[i]]
-        )]
-    return points
+def get_hilbert_curve(gray: np.ndarray, max_depth=8):
+    def hilbert_recursive(point, size, depth = 0, offsets = OFFSETS):
+        points = [(point[0] + size / 4 * o0, point[1] + size / 4 * o1) for o0, o1 in offsets]
+        image_slice = gray[
+            round(point[1] - size / 2):round(point[1] + size / 2),
+            round(point[0] - size / 2):round(point[0] + size / 2),
+        ]
+        darkness = (255 - np.min(image_slice)) / 255
+        if depth / max_depth < np.sqrt(darkness):
+            return [h for i, p in enumerate(points) for h in hilbert_recursive(
+                p, size / 2, depth + 1, [offsets[j] for j in ORDERS[i]]
+            )]
+        return points
 
 
-def get_hilbert_curve(gray: np.ndarray):
     size = gray.shape[0]
-    points = np.array(hilbert_recursive((size/2, size/2), size, gray))
+    points = np.array(hilbert_recursive((size/2, size/2), size))
     return points.reshape((-1, 2))
 
 
@@ -79,12 +78,12 @@ def evaluate(contour, image):
     return np.mean(np.abs(canvas - image))
 
 
-def get_crow_curve(gray: np.ndarray, points=400, N=2000):
+def get_crow_curve(gray: np.ndarray, points=300, iterations=20000):
     size = gray.shape[0]
     contour = np.random.randint(0, size, size=(points, 2))
     error = evaluate(contour, gray)
     magnitude = size/3
-    progressbar = tqdm(range(N))
+    progressbar = tqdm(range(iterations))
     for i in progressbar:
         mask = np.zeros((points, 1))
         mask[np.random.randint(0, points)] = 1
@@ -100,17 +99,18 @@ def get_crow_curve(gray: np.ndarray, points=400, N=2000):
     return contour
 
 
-methods = {"crow": get_crow_curve, "hilbert": get_hilbert_curve, "contour": get_contour}
+methods = {"crow": get_crow_curve, "am": get_am_line, "hilbert": get_hilbert_curve, "contour": get_contour}
 
 
-@click.command()
+@click.command(context_settings=dict(ignore_unknown_options=True))
 @click.argument("image", type=click.Path(exists=True))
 @click.option("--image-size", default=1024)
 @click.option("--method", default="crow", type=click.Choice(methods.keys()))
 @click.option("--serial-port", default="/dev/ttyACM0", type=click.Path(exists=True))
-def cli(image: str, image_size: int, method: str, serial_port: str):
+@click.option("--method_json", type=str)
+def cli(image: str, image_size: int, method: str, serial_port: str, method_json: str):
     gray = read_gray(image, image_size)
-    curve = methods[method](gray)
+    curve = methods[method](gray, **json.loads(method_json))
     draw_contour(curve, image_size)
     curve = contour_to_canvas(curve, image_size)
     with serial.Serial(port=serial_port, baudrate=9600) as arduino:
